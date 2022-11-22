849679d5ceed2a956a0dac32852411b99ed8ff2d
juan pablo rojas <64866661+dehydratedxdd@users.noreply.github.com>
2022-11-22 14:28:02 -0500

    2

--- legal/src/test/AppTest.java
+++ legal/src/test/AppTest.java
@@ -6,13 +6,13 @@ public class  App {
       System.out.println(evaluate("2+3+4"));
     }
     
-    // adds two ints
+    
     public static int sum(int a, int b) {
       int sum = a + b;
       return sum;
     }
  
-    // calculates an additional expression
+    // Se calculan las expresiones para el test
     public static int evaluate(String expression) {
       int sum = 0;
       for (String summand: expression.split("\\+"))
