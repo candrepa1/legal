e211e160ab45b8d3cc9e882c180947ef80bacdb0
juan pablo rojas <64866661+dehydratedxdd@users.noreply.github.com>
2022-11-22 15:55:46 -0500

    test

--- legal/src/test/AppTest.java
+++ legal/src/test/AppTest.java
@@ -1,22 +1,32 @@
 package com.legal;
  
-public class  App {
-    public static void main(String[] args) {
-      System.out.println(sum(1,2));
-      System.out.println(evaluate("2+3+4"));
-    }
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api*;
+
+public class AppTest
+{
+
+@BeforeAll
+public static void runBeforeClass(){
+    System.out.println("Runs before entire test suite");
+}
+@BeforeEach
+public void runBeforeEachTest(){
+    System.out.println("Runs Before each test");
+}
+@Disable
+public void thisTestIsSkipped(){
     
-    
-    public static int sum(int a, int b) {
-      int sum = a + b;
-      return sum;
-    }
- 
-    // Se calculan las expresiones para el test
-    public static int evaluate(String expression) {
-      int sum = 0;
-      for (String summand: expression.split("\\+"))
-        sum += Integer.valueOf(summand);
-      return sum;
-    }
-  }
+}
+@Test
+public void shouldAnswerWithTrue()
+{
+    assertTrue( true);
+}
+@Test
+public void assertDemo(){
+    int expected = 10;
+    int actual = -10;
+    assertEquals("asserDemo fails", expected, actual);
+}}
+
